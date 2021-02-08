9bd91270bc0c6151b07ef6ef24878759ea8564e8
yukilost <yukilost42@gmail.com>
2021-02-08 13:45:35 +0900

    solve ABC064

---
+++ ABC/064/C.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, n)   for(int i = 0; i < (n); i++)
+#define REPS(i, n)  for(int i = 1; i <= (n); i++)
+#define RREP(i, n)  for(int i = (n)-1; i >= 0; i--)
+#define RREPS(i, n) for(int i = (n); i > 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end())
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+
+using ll  = long long;
+using pi  = pair<int, int>;
+using pl  = pair<ll, ll>;
+using vi  = vector<int>;
+using vl  = vector<ll>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
+const ll  INFL = 1e18;
+
+template<class T> bool chmax(T &a, const T &b) { if(a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if(a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(10);
+    
+    int N; cin >> N;
+    int num[9]; REP(i, 9) num[i] = 0;
+    REP(i, N)
+    {
+        int a; cin >> a;
+        num[min(8, a/400)]++;
+    }
+
+    if(num[8] == N)
+    {
+        cout << 1 << " " << num[8] << endl;
+    }
+    else
+    {
+        int s = 0;
+        REP(i, 8) if(num[i] > 0) s++;
+        cout << s << " " << s+num[8] << endl;
+    }
+}
