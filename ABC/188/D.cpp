8c8aa0028fcd8372e74b4bf4e50f4d8eb2d27e7a
yukilost <yukilost42@gmail.com>
2021-04-01 12:44:47 +0900

    solve ABC188.D

---
+++ ABC/188/D.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, n)   for(int i = 0; i < (n); i++)
+#define REPS(i, n)  for(int i = 1; i <= (n); i++)
+#define RREP(i, n)  for(int i = (n)-1; i >= 0; i--)
+#define RREPS(i, n) for(int i = (n); i > 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end())
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+
+using ll  = long long;
+using pi  = pair<int, int>;
+using pl  = pair<ll, ll>;
+using vi  = vector<int>;
+using vl  = vector<ll>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
+const ll  INFL = 1e18;
+const double PI = 3.14159265;
+
+template<class T> bool chmax(T &a, const T &b) { if(a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if(a > b) { a = b; return true; } return false; }
+
+int main()
+{ 
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+
+    ll N, C; cin >> N >> C;
+    vector<pl> p;
+    REP(i, N)
+    {
+        ll a, b, c; cin >> a >> b >> c;
+        p.pb(mp(a, c));
+        p.pb(mp(b+1, -c));
+    }
+    sort(ALL(p));
+    ll s = p[0].first;
+    ll cost = p[0].second;
+    ll ans = 0;
+    REPS(i, (int)p.size()-1)
+    {
+        ll t = p[i].first;
+        ans += min(C, cost)*(t-s);
+        s = t;
+        cost += p[i].second;
+    }
+    cout << ans << endl;
+}
