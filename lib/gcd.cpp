838b9b32094265f190ff18c8eca8140ff82d7175
yukilost <yukilost42@gmail.com>
2021-04-23 10:24:13 +0900

    add extgcd

--- lib/gcd.cpp
+++ lib/gcd.cpp
@@ -10,3 +10,11 @@ ll lcm(ll a, ll b)
 {
     return a/gcd(a, b)*b;
 }
+
+ll extgcd(ll a, ll b, ll &x, ll &y) {
+    ll g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) g = extgcd(b, a % b, y, x), y -= (a / b) * x;
+    return g;
+}
