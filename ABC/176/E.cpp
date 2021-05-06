6e8759852d5ae6f1705e0b4eb3eec0445ebf3673
yukilost <yukilost42@gmail.com>
2021-05-06 11:01:58 +0900

    solve ABC176.E

---
+++ ABC/176/E.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, n)   for(int i = 0; i < (n); i++)
+#define REPS(i, n)  for(int i = 1; i <= (n); i++)
+#define RREP(i, n)  for(int i = (n)-1; i >= 0; i--)
+#define RREPS(i, n) for(int i = (n); i > 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end())
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+
+using ll  = long long;
+using pi  = pair<int, int>;
+using pl  = pair<ll, ll>;
+using vi  = vector<int>;
+using vl  = vector<ll>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+const int MOD = 1e9 + 7;
+//const int MOD = 998244353;
+const int INF = 1e9 + 7;
+const ll  INFL = 1e18;
+const double PI = 3.14159265359;
+
+template<class T> bool chmax(T &a, const T &b) { if(a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if(a > b) { a = b; return true; } return false; }
+
+int main()
+{ 
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+
+    int H, W, M; cin >> H >> W >> M;
+    map<int, int> ch, cw;
+    int mh = 0, mw = 0;
+    set<pi> s;
+    REP(i, M)
+    {
+        int h, w;
+        cin >> h >> w;
+        s.insert(mp(h, w));
+        chmax(mh, ++ch[h]);
+        chmax(mw, ++cw[w]);
+    }
+
+    vi ih, iw;
+    for(auto p: ch)
+    {
+        if(mh == p.second) ih.pb(p.first);
+    }
+    for(auto p: cw)
+    {
+        if(mw == p.second) iw.pb(p.first);
+    }
+
+    int ans = mh+mw-1;
+    for(auto h: ih)
+    {
+        for(auto w: iw)
+        {
+            if(s.find(mp(h, w)) == s.end())
+            {
+                ans++;
+                cout << ans << endl;
+                return 0;
+            }
+        }
+    }
+    cout << ans << endl;
+}
