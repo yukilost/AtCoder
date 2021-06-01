f27348951057ee1d7a516c7bef99794dd9423aee
yukilost <yukilost42@gmail.com>
2021-06-01 21:50:41 +0900

    fix

--- lib/prime.cpp
+++ lib/prime.cpp
@@ -2,7 +2,7 @@
 
 const int MAX = 1e7;  // 最大値
 int prime[MAX];       // i番目の素数
-bool is_prime[MAX+1]; // trueなら素数
+int divisor[MAX+1];   // iを割り切る最大の素数
 
 /**
  * @fn sieve
@@ -13,14 +13,13 @@ bool is_prime[MAX+1]; // trueなら素数
 int sieve(int n)
 {
     int p = 0;
-    for(int i = 0; i <= n; i++) is_prime[i] = true;
-    is_prime[0] = is_prime[1] = false;
+    for(int i = 0; i <= n; i++) divisor[i] = i;
     for(int i = 2; i*i <= n; i++)
     {
-        if(prime[i])
+        if(divisor[i] == i)
         {
             prime[p++] = i;
-            for(int j = 2*i; j <= n; j+=i) is_prime[j] = false;
+            for(int j = 2*i; j <= n; j+=i) divisor[j] = i;
         }
     }
 }
